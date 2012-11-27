b20d20e999fe7e9770eb687b0cee8e07092c83ed
Federico Builes <federico.builes@gmail.com>
2012-11-26 21:26:55 -0500

    Get rid of unnecesary functions.

--- parser.erl
+++ parser.erl
@@ -2,16 +2,8 @@
 -export([get_lyrics/2]).
 
 get_lyrics(Artist, Title) ->
-    [Content|_T] = get_textarea_content(Artist, Title),
-    case re:run(Content, "lyrics>(?<LYRIC>.*?)&lt", [dotall, {capture, ["LYRIC"], list}]) of
+    Text = wikia:get_wikia_content(Artist, Title),
+    case re:run(Text, "lyrics>(?<LYRIC>.*?)&lt", [dotall, {capture, ["LYRIC"], list}]) of
         { match, Captures } -> io:format("~s", [hd(Captures)]);
         nomatch -> "No Matches"
     end.
-
-get_textarea_content(Artist, Title) ->
-    Text = wikia:get_wikia_content(Artist, Title),
-    case re:run(Text, "wpTextbox1.*?>(.*?)</textarea>", [dotall, {capture, all, list}]) of
-        { match, Captures } -> tl(Captures);
-        nomatch -> "No Matches"
-    end.
-
